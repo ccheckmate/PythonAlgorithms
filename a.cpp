b449ed3a31d4347074a1551812bcd48cce6e1716
icecrak <45572748+icecrak@users.noreply.github.com>
2020-10-10 13:58:05 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(int i,int n)
+{
+    if(fun)
+    if(s[i-1]==1)
+    {
+        s
+    }
+        fun(i+1,n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    fun(0,n)
+}
